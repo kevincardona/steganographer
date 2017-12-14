7881a755c9f6da8d6af474e4159ab1a8cd7b298e
kevincardona <kevincardona1@gmail.com>
2017-12-14 11:48:07 -0500

    Fixed major issues with image processing!

--- steg.py
+++ steg.py
@@ -14,28 +14,37 @@ def decode():
 
     # Available storage bits (3 (RGB) * width * height * max_lsb)
     max_bits = 3 * image.size[0] * image.size[1] * max_lsb
-    image_data = list(image.convert("RGBA").getdata())
-
-    size = ""
-    # Gets the size of the hidden message
-    for i in range(8):
-        size += str(image_data[i][0] & 1)
-        size += str(image_data[i][1] & 1)
-        size += str(image_data[i][2] & 1)
-    size = int(size,2)
+    image_data = image.convert("RGB").getdata()
 
     data = ""
-    for i in range(size/3 + 1):
-        data += str(image_data[i + 8][0] & 1)
-        if (i * 3 + 2 <= size):
-            data += str(image_data[i + 8][1] & 1)
-        if (i * 3 + 3 <= size):
-            data += str(image_data[i + 8][2] & 1)
+    ssize = ""
+    size = 0
+    count = 0
+    for h in range(image.height):
+        for w in range(image.width):
+            (r, g, b) = image_data.getpixel((w,h))
+            if (h*image.height + w < 8):
+                ssize += str(r & 1)
+                ssize += str(g & 1)
+                ssize += str(b & 1)
+            else:
+                if (h*image.height + w == 8):
+                    size = int(ssize,2)
+
+                if (w + image.height * h < size+8):
+                    if ((count < size)):
+                        data += str(r & 1)
+                    if (count + 1 < size):
+                        data += str(g & 1)
+                    if (count + 2 < size):
+                        data += str(b & 1)
+                    count += 3
+
 
     # Convert lsb data back into message
     data = hex(int(data,2))[2:]
-    data = data[:len(data)-1]
-    print "Message: %s" % binascii.unhexlify(data)
+    data = data.strip('L')
+    print "Message:\n%s\n" % binascii.unhexlify(data)
 
 def encode():
     global image, imagepath
@@ -55,28 +64,27 @@ def encode():
 
     # Available storage bits (3 (RGB) * width * height * max_lsb)
     max_bits = 3 * image.size[0] * image.size[1] * max_lsb
-    image_data = list(image.getdata())
+    image_data = image.convert("RGB").getdata()
 
     new_image = Image.new('RGB', (image.width, image.height));
     new_image_data = new_image.getdata()
 
     data_iterator = 0
-    for i in range(image.height * image.width):
-            r = image_data[i][0]
-            g = image_data[i][1]
-            b = image_data[i][2]
+    for h in range(image.height):
+        for w in range(image.width):
+            (r, g, b) = image_data.getpixel((w,h))
             r = r >> 2
             r = r << 2
             g = g >> 2
             g = g << 2
             b = b >> 2
             b = b << 2
-            if (i < 8):
-                if (i * 3 < len(binsize)):
-                    r += int (binsize[i * 3])
-                    g += int (binsize[i * 3 + 1])
-                    b += int(binsize[i * 3 + 2])
-            if ((i >= 8) & (data_iterator < len(bintext))):
+            if (h * image.height + w < 8):
+                if (w + h < len(binsize)):
+                    r += int (binsize[(w+h) * 3])
+                    g += int (binsize[(w+h) * 3 + 1])
+                    b += int(binsize[(w+h) * 3 + 2])
+            if ((h * image.height + w >= 8) & (data_iterator < len(bintext))):
                 r += int(bintext[data_iterator])
                 if (data_iterator + 1 < len(bintext)):
                     g += int(bintext[data_iterator + 1])
@@ -84,7 +92,7 @@ def encode():
                     b += int(bintext[data_iterator + 2])
                 data_iterator += 3
 
-            new_image_data.putpixel((i % image.height, int (math.floor(i/image.height)) ), (r, g, b))
+            new_image_data.putpixel((w,h), (r, g, b))
 
     print "Saving image in steg-images/"
     new_image.save( "steg-images/" + os.path.splitext(os.path.basename(sys.argv[2]))[0] + ".png" , "PNG")
