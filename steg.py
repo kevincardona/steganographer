98d6b99f9bfd3aef80c9da687d92bf0361a5e640
kevincardona <kevincardona1@gmail.com>
2017-12-13 20:40:29 -0500

    Removed encode.py, using steg.py for all functions

--- steg.py
+++ steg.py
@@ -11,24 +11,25 @@ def encode():
     bintext = bin(int(binascii.hexlify(rawtext),16))
     bintext = bintext[:1] + bintext[2:]
     print bintext
-    try:
-        imagepath = sys.argv[2]
-        image = Image.open(imagepath, "rb")
-    except:
-        print "Input image not found."
-        sys.exit(2)
+    imagepath = sys.argv[2]
+    image = Image.open(imagepath)
 
-        # Available storage bits (3 (RGB) * width * height * max_lsb)
-        max_bits = 3 * image.size[0] * image.size[1] * max_lsb
-        image_data = list(image_data.getdata())
-        data_iterator = 0
 
-        
-        working = True
+    # Available storage bits (3 (RGB) * width * height * max_lsb)
+    max_bits = 3 * image.size[0] * image.size[1] * max_lsb
+    image_data = list(image.getdata())
+    data_iterator = 0
+
+    working = True
+    x = 5;
+    print "x : %d" % x
+    x = x >> 2
+    x = x << 2
+    print "x2 : %d" % x
 #        while (working):
 #            rgb = list(image_data[data_iterator])
 #            for i in range(3):
-
+    
 
 
 
@@ -48,7 +49,6 @@ if __name__ == "__main__":
 
     if (option == "encode"):
         encode()
-        assemble()
         sys.exit(1)
     else:
         print "TODO: Implement decode"
